65f9da2a0fb68e41f862105c96e40ad572405e0d
Max Fisher <max@maxhfisher.com>
2020-11-13 23:21:11 -0500

    Initial Commit

---
+++ Shared/DoDateApp.swift
@@ -0,0 +1,20 @@
+//
+//  DoDateApp.swift
+//  Shared
+//
+//  Created by Max Fisher on 11/13/20.
+//
+
+import SwiftUI
+
+@main
+struct DoDateApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
