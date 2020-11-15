3990a0604f411ac7f33c72820cf26b8cbd2cacba
Max Fisher <66274614+maxhfisher@users.noreply.github.com>
2020-11-15 01:17:52 -0500

    create model class

--- Shared/Model.swift
+++ Shared/Model.swift
@@ -6,5 +6,32 @@
 //
 
 import Foundation
+import CoreData
 
-//TODO: Make Model Class
+class Model: ObservableObject {
+	@Published var projects: [Project]
+	@Published var dueDates: [DueDate]
+	@Published var doDates: [DoDate]
+	
+	init() {
+		do {
+			try projects = Project.fetchRequest().execute()
+		} catch {
+			projects = []
+		}
+		do {
+			let dueDatesFetchRequest: NSFetchRequest<DueDate> = DueDate.fetchRequest()
+			dueDatesFetchRequest.sortDescriptors = [NSSortDescriptor(keyPath: \DueDate.date, ascending: true)]
+			try dueDates = dueDatesFetchRequest.execute()
+		} catch {
+			dueDates = []
+		}
+		do {
+			let doDatesFetchRequest: NSFetchRequest<DoDate> = DoDate.fetchRequest()
+			doDatesFetchRequest.sortDescriptors = [NSSortDescriptor(keyPath: \DoDate.date, ascending: true)]
+			try doDates = doDatesFetchRequest.execute()
+		} catch {
+			doDates = []
+		}
+	}
+}
--- Shared/ToDoView.swift
+++ Shared/ToDoView.swift
@@ -27,41 +27,7 @@ struct ToDoView: View {
 				}
 			}
 		}
-		.onAppear(perform: sortDays)
     }
-	
-	func sortDays() {
-		var sortedDays: [Day] = []
-		var dates: [Date] = []
-
-		for doDate in doDates {
-			guard doDate.date != nil else { continue }
-
-			if dates.contains(doDate.date!) {
-				sortedDays.first(where: { doDate.date == $0.date })!.doDates.append(doDate)
-			} else {
-				dates.append(doDate.date!)
-
-				let newDay = Day(forDate: doDate.date!)
-				newDay.doDates.append(doDate)
-				sortedDays.append(newDay)
-			}
-		}
-
-		for dueDate in dueDates {
-			guard dueDate.date != nil else { continue }
-
-			if dates.contains(dueDate.date!) {
-				sortedDays.first(where: { dueDate.date == $0.date })!.dueDates.append(dueDate)
-			} else {
-				dates.append(dueDate.date!)
-
-				let newDay = Day(forDate: dueDate.date!)
-				newDay.dueDates.append(dueDate)
-				sortedDays.append(newDay)
-			}
-		}
-	}
 	
 	struct PlusButtonView: View {
 		var body: some View {
@@ -82,16 +48,6 @@ struct ToDoView: View {
 				.background(Color.pink)
 				.clipShape(Circle())
 			}
-		}
-	}
-
-	class Day {
-		let date: Date
-		var doDates: [DoDate] = []
-		var dueDates: [DueDate] = []
-
-		init(forDate date: Date) {
-			self.date = date
 		}
 	}
 }
