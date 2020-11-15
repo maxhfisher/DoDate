7f62c277a32594accc497c6539c66fd622bd1dfe
Max Fisher <66274614+maxhfisher@users.noreply.github.com>
2020-11-15 17:17:26 -0500

    add support for creating projects

--- Shared/ProjectsView.swift
+++ Shared/ProjectsView.swift
@@ -8,23 +8,77 @@
 import SwiftUI
 
 struct ProjectsView: View {
-	@EnvironmentObject var model: Model
+	@FetchRequest(entity: Project.entity(), sortDescriptors: []) var projects: FetchedResults<Project>
+	
+	@State private var showingAddProjectSheet = false
 	
     var body: some View {
 		NavigationView {
-			List(model.projects, id: \.self) { project in
+			List(projects, id: \.self) { project in
 				Text(project.entity.name ?? "")
 			}
 			.navigationTitle("Projects")
-			.navigationBarItems(trailing: Button {} label: {
+			.navigationBarItems(trailing: Button { showingAddProjectSheet = true } label: {
 				Image(systemName: "plus")
 			})
+			.sheet(isPresented: $showingAddProjectSheet, content: { NewProjectView() })
 		}
     }
 }
 
+struct NewProjectView: View {
+	@Environment(\.managedObjectContext) var context
+	@Environment(\.presentationMode) var presentationMode
+	
+	@State private var title = ""
+	@State private var details = ""
+	
+	@State private var showingErrorAlert = false
+	
+	var body: some View {
+		NavigationView {
+			Form {
+				Section {
+					TextField("Name", text: $title)
+					TextField("Details", text: $details)
+				}
+				
+				Section {
+					HStack {
+						Spacer()
+						Button("Save") {
+							if title.isEmpty || details.isEmpty {
+								//TODO
+							} else {
+								do {
+									let newProject = Project(context: context)
+									newProject.name = title
+									newProject.details = details
+									
+									
+									
+									try context.save()
+								} catch {
+									showingErrorAlert = true
+								}
+							}
+						}
+						.alert(isPresented: $showingErrorAlert, content: {
+							Alert(title: Text("There was an error saving your project"))
+						})
+						Spacer()
+					}
+				}
+			}
+			.navigationBarTitleDisplayMode(.inline)
+			.navigationTitle("New Project")
+			.navigationBarItems(trailing: Button("Cancel") { presentationMode.wrappedValue.dismiss() })
+		}
+	}
+}
+
 struct ProjectsView_Previews: PreviewProvider {
     static var previews: some View {
-		ProjectsView().environmentObject(Model())
+		ProjectsView()
     }
 }
