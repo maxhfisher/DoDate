c519ad84bd4fb57b1b180f56f29611391774a887
Max Fisher <66274614+maxhfisher@users.noreply.github.com>
2020-11-22 14:12:42 -0500

    add project selection view

--- Shared/DoDatesView.swift
+++ Shared/DoDatesView.swift
@@ -8,15 +8,62 @@
 import SwiftUI
 
 struct DoDatesView: View {
+	@Environment(\.managedObjectContext) var context
+	
+	@FetchRequest(entity: DoDate.entity(), sortDescriptors: []) var doDates: FetchedResults<DoDate>
+	
+	@State private var showingNewDoDateView = false
+	
     var body: some View {
 		NavigationView {
-			Text("Do Dates")
-				.navigationTitle("Do Dates")
-				.navigationBarItems(trailing: Button {} label: {
-					Image(systemName: "plus")
-				})
+			Group {
+				if doDates.isEmpty {
+					Text("No Do Dates Yet")
+						.font(.largeTitle)
+						.foregroundColor(.secondary)
+				} else {
+					List {
+						ForEach(doDates, id: \.id) { doDate in
+							Text(doDate.details ?? "")
+						}
+					}
+				}
+			}
+			.navigationTitle("Do Dates")
+			.navigationBarItems(leading: EditButton(), trailing: Button { showingNewDoDateView = true } label: {
+				Image(systemName: "plus")
+			})
+			.sheet(isPresented: $showingNewDoDateView, content: { NewDoDatesView() })
 		}
     }
+}
+
+struct NewDoDatesView: View {
+	@Environment(\.managedObjectContext) var context
+	
+	@FetchRequest(entity: Project.entity(), sortDescriptors: [NSSortDescriptor(keyPath: \Project.name, ascending: true)]) var projects: FetchedResults<Project>
+	
+	@State private var projectSelection = 0
+	@State private var dueDateSelection = 0
+	
+	var body: some View {
+		NavigationView {
+			Form {
+				Section {
+					ProjectSelectionView(projects: Array(projects), selectionIndex: $projectSelection)
+				}
+			}
+			.navigationBarTitleDisplayMode(.inline)
+			.navigationTitle("New Do Date")
+		}
+	}
+	
+	func formatDate(_ date: Date) -> String {
+		let dateFormatter = DateFormatter()
+		dateFormatter.dateStyle = .short
+		
+		return dateFormatter.string(from: date)
+	}
 }
 
 struct DoDatesView_Previews: PreviewProvider {
--- Shared/ProjectsView.swift
+++ Shared/ProjectsView.swift
@@ -101,6 +101,56 @@ struct NewProjectView: View {
 	}
 }
 
+struct ProjectSelectionView: View {
+	@Environment(\.managedObjectContext) var context
+	
+	let projects: [Project]
+	@Binding var selectionIndex: Int
+	
+	@State private var showingAddProjectView = false
+	
+	var body: some View {
+		NavigationLink( destination:
+			Form {
+				Section {
+					ForEach(0..<projects.count) { i in
+						HStack {
+							Text(projects[i].name ?? "")
+							Spacer()
+							if selectionIndex == i {
+								Image(systemName: "checkmark")
+									.foregroundColor(selectionIndex == i ? .accentColor:.primary)
+							}
+						}
+						.onTapGesture {
+							selectionIndex = i
+						}
+						.accessibilityElement(children: .ignore)
+						.accessibility(label: Text(projects[i].name ?? ""))
+						.accessibility(addTraits: selectionIndex == i ? [.isButton, .isSelected]:[.isButton])
+					}
+				}
+				Section {
+					Button("Add New Project") {
+						showingAddProjectView = true
+					}
+				}
+			}
+			.navigationTitle("Select a Project")
+			.sheet(isPresented: $showingAddProjectView, content: { NewProjectView().environment(\.managedObjectContext, context) })
+			) {
+				HStack {
+					Text("Projects")
+					Spacer()
+					if !projects.isEmpty {
+						Text(projects[selectionIndex].name ?? "")
+							.foregroundColor(.secondary)
+					}
+				}
+			}
+	}
+}
+
 struct ProjectsView_Previews: PreviewProvider {
     static var previews: some View {
 		ProjectsView()
